b9115a682a6b9c1b9a629e5be08ac4729c543cdc
Matias <matias.sanchez.1897@hotmail.com>
2026-06-10 18:53:26 -0300

    realtime notifications is active

--- app/Notifications/JoinRequestReceived.php
+++ app/Notifications/JoinRequestReceived.php
@@ -4,12 +4,12 @@ namespace App\Notifications;
 
 use App\Models\JoinRequest;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class JoinRequestReceived extends Notification implements ShouldBroadcast
+class JoinRequestReceived extends Notification implements ShouldBroadcastNow
 {
     use Queueable;
 
